ac4406ccd1a797a00515782dbbc1ba7e2f85fc88
MisterPrada <lesha.skorpion@gmail.com>
2023-02-02 11:49:52 +0300

    Add Metrics
    Co-authored-by: GarrettPierrotLeFou <valentine.deruzhinsky@gmail.com>

--- index.php
+++ index.php
@@ -6,13 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Death of the Sun</title>
+    <meta name="description" content="Remaining time before the death of the sun"/>
+    <meta name="keywords" content="doomsday|apocalypse|sun|deathofthesun"/>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 <div class="time">
     <time></time>
+    <label>Seconds left until the sun dies</label>
 </div>
 
 <script src="/js/app.js" type="text/javascript"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HV8ST64VB6"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HV8ST64VB6');
+</script>
 </body>
 </html>
